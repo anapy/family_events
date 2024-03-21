5b47d26794d3a7da7a40e2fd146df14dee2c4ccc
anapy <anabelenfunes@gmail.com>
2024-03-21 12:58:54 +0100

    feat: create first commit for mobile-app

---
+++ front-project/folkaApp/App.tsx
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+      <View
+        style={styles.sectionContainer}>
+        <Text>
+          Hola Mundo
+        </Text>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  }
+});
+
+export default App;
